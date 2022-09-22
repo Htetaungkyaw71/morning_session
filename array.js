3f01d06f595f70f24de09992e81d9e45d61663ae
Htetaungkyaw71 <htetaung200071@gmail.com>
2022-09-22 13:58:07 +0630

    initial commit

---
+++ array.js
@@ -0,0 +1,34 @@
+// Creating Array / Acesssing Array
+
+// Now Changing Array
+
+// 1. Array are mutable
+
+// Start Changing the value of array
+let colors = ["red","green","blue"]
+
+
+
+// Array methods (push,pop,unshift,shift)
+
+// push - add to end
+
+
+
+// pop - remove to end
+
+
+
+// unshift - add to start
+
+// shift - remove to start
+
+
+//indexOf
+
+
+//includes 
+
+
+
+//concate

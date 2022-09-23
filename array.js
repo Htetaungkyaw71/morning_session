9ac40f4e8d03661c85144a592476d6236d5b803a
Htetaungkyaw71 <htetaung200071@gmail.com>
2022-09-23 13:56:38 +0630

    initial commit

--- array.js
+++ array.js
@@ -7,28 +7,68 @@
 // Start Changing the value of array
 let colors = ["red","green","blue"]
 
+// let str = "hello"
+// str[0] = "H";
+// console.log(str)
+
+// colors[0] = "yellow"
+// console.log(colors)
+
+// colors[colors.length] = "black"
+// console.log(colors)
+
+
 
 
 // Array methods (push,pop,unshift,shift)
 
+
 // push - add to end
 
+// colors.push("yellow","two","ther",true,313)
+// console.log(colors)
 
 
 // pop - remove to end
 
+// colors.pop()
+// console.log(colors)
 
 
 // unshift - add to start
 
+// colors.unshift("gray")
+// console.log(colors)
+
 // shift - remove to start
+
+// colors.shift()
+// console.log(colors)
+
 
 
 //indexOf
 
+// let index = colors.indexOf("hadhadhdah")
+// console.log(index)
+
+// if(colors.indexOf("red") !== -1 ){
+//     console.log("found")
+// }
+
 
 //includes 
 
+// const res = colors.includes("rsaaded")
+// console.log(res)
 
 
 //concate
+
+// let num = [2,4,5,6]
+// let str = ["a","b","c"]
+
+// let res = num.concat(str)
+// console.log(res)
+// console.log(num)
+// console.log(str)
